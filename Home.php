da5f607dac4984c7dd05bcf1d872833450805f25
Charli_x1 <derrhalo50@gmail.com>
2024-03-03 18:11:29 +0100

    yarbe tkhdem

--- Home.php
+++ Home.php
@@ -19,6 +19,49 @@
         header('location: Home.php');
 
     }
+
+    $post_creation_time_str = $posts[0]["Created_at"];
+
+    // $post_creation_time = new DateTime($post_creation_time_str);
+
+
+    // $current_time = new DateTime();
+
+
+    // $time_difference = $current_time->diff($post_creation_time);
+
+    // echo "Time difference: " . $time_difference->format("%d days, %h hours, %i minutes, %s seconds");
+
+    // $post_creation_time_str = $posts[0]["Created_at"];
+
+    // // Convert the string to a DateTime object
+    // $post_creation_time = new DateTime($post_creation_time_str);
+
+    // $current_time = new DateTime();
+
+    // // Calculate the time difference
+    // $time_difference = $current_time->diff($post_creation_time);
+
+    // $formatted_time_difference = '';
+
+    // if ($time_difference->d > 0) {
+    //     $formatted_time_difference .= $time_difference->d . ' days ';
+    // }
+
+    // if ($time_difference->h > 0) {
+    //     $formatted_time_difference .= $time_difference->h . ' hours ';
+    // }
+
+    // if ($time_difference->i > 0) {
+    //     $formatted_time_difference .= $time_difference->i . ' minutes ';
+    // }
+
+    // if ($time_difference->s > 0 || empty($formatted_time_difference)) {
+    //     $formatted_time_difference .= $time_difference->s . ' seconds';
+    // }
+
+    // echo $formatted_time_difference;
+
 ?>
 
 <!DOCTYPE html>
@@ -26,23 +69,40 @@
     <!-- for blur in delete-->
     <div class="overlay"></div>
     <!-- Start The Add Post Pop Up -->
-    <form class="pop-up py-5 container-fluid primary-colors d-flex justify-content-center align-items-center flex-column gap-4 d-none " action="" method="POST">
+    <form class="pop-up py-5 container-fluid primary-colors d-flex justify-content-center align-items-center flex-column gap-4 d-none " action="Home.php" method="POST">
         <h1 class="mb-3 btn_p">Add Your Post</h1>
         <div class="parent d-flex flex-row w-100">
             <div class="cont d-flex flex-column align-items-center w-100">
                 <label for="Name" class="fs-4">Full Name: </label>
-                <input type="text" class="w-75 " placeholder="Full Name">
-            </div>
-            <div class="cont d-flex  flex-column align-items-center w-100 ">
-                <label for="Email" class="fs-4">Email: </label>
-                <input type="text" class="w-75 " placeholder="Email">
+                <input type="text" class="w-75 " required name="name" placeholder="Full Name" value="">
             </div>
         </div>
         <div class="the-post">
-            <textarea name="post" id="post" cols="80" rows="5" placeholder="Express Yourself Freely!" class="w-100 "></textarea>
+            <textarea name="body" cols="80" rows="5" required placeholder="Express Yourself Freely!" class="w-100 " value=""></textarea>
+        </div>
+        <div class="text-center">
+            <input type="submit" name="submit" value="Submit" class="btn btn-secondary fs-4 px-4" style="color: white !important; ">
         </div>
     </form>
     <!-- End The Add Post Pop Up -->
+    <!-- Start The Edit Post Pop Up -->
+    <form class="edit-pop-up py-5 container-fluid primary-colors d-flex justify-content-center align-items-center flex-column gap-4 d-none " action="" method="POST">
+        <h1 class="mb-3 btn_p">Edit Your Post</h1>
+        <div class="parent d-flex flex-row w-100">
+            <div class="cont d-flex flex-column align-items-center w-100">
+                <label for="Name" class="fs-4">Edit The Name Here: </label>
+                <input type="text" class="w-75 " placeholder="Edit Your Name">
+            </div>
+        </div>
+        <div class="the-post">
+            <textarea name="post" id="post" cols="79" rows="5" placeholder="Edit The Post Here!" class="w-100 "></textarea>
+        </div>
+        <div class="text-center">
+            <input type="submit" name="submit" value="Submit" class="btn btn-secondary fs-4 px-4 " style="color: white !important; ">
+            <button class="btn btn-secondary text-danger fs-4 px-4">Cancel</button>
+        </div>
+    </form>
+    <!-- End The Edit Post Pop Up -->
 
     <div class="con" style="position: static;">
         <?php require "./components/header.php"; ?>
@@ -86,22 +146,17 @@
                     <div class="prt1">
                         <div class="name fw-bolder fs-5"><?php echo htmlspecialchars($post["name"])?></div>
                         <div class="date text-start"><?php echo htmlspecialchars($formatted_time_difference)?></div>
-            <div class="row card p-3 gap-3">
-                <div class="col-12 d-flex justify-content-between">
-                    <div class="prt1">
-                        <div class="name fw-bolder fs-5">George Lobko</div>
-                        <div class="date text-start">2 Hours Ago</div>
                     </div>
                     <div class="brdr border d-flex justify-content-center ">
                         <i class="text-center fa-solid fs-3 fa-ellipsis-vertical align-items-center d-flex"></i>
                         <div class="menu">
-                            <div class="bg-first border-bottom border-secondary">Edit</div>
+                            <div class="bg-first border-bottom border-secondary edit">Edit</div>
                             <div class="bg-first delete">Delete</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-12 fw-light fs-6" style="font-size: 18.5px !important;">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae alias dignissimos voluptatum, iste et ab, fuga corporis nisi doloribus, explicabo deleniti. Debitis labore neque autem animi illo asperiores, blanditiis pariatur aliquam praesentium maxime nisi consequatur? Veritatis quia repudiandae voluptatibus incidunt ipsa, magnam eligendi sint odio quo possimus, deleniti porro quae. Alias omnis exercitationem libero aut fugit necessitatibus esse beatae nam minima quo, aliquid perspiciatis soluta cumque iusto! Ea nam nobis iusto repellendus sapiente itaque natus. Minima pariatur sed, debitis ipsum qui voluptate quod. Ad obcaecati sapiente distinctio quia, fugiat inventore, impedit facilis tempora, ratione libero mollitia optio vel beatae deserunt!
+                    <?php echo htmlspecialchars($post["body"])?>
                 </div>
                 <div style="width: fit-content !important;" class="fs-5"><div class="btnn "><i class="fa-regular fa-heart"></i>  Like</div></div>
             </div>
