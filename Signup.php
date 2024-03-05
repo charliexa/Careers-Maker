b7e5d9f9e87f5c5f2e500ec9e82d49e27a93d5a2
yahya akli <yahyagamer2005@gmail.com>
2024-03-05 22:35:36 +0100

    1

---
+++ Signup.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/Signup.css">
+    <title>Signe up</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h1>Sing up</h1>
+        <div>
+            <div>
+                <fieldset>
+                    <legend>Full Name</legend>
+                    <input type="text" name="name" id="name">
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Email</legend>
+                    <input type="email" name="email" id="email">
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Password</legend>
+                    <input type="password" name="password" id="password">
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Confirm password</legend>
+                    <input type="password" name="Cpassword" id="Cpassword">
+                </fieldset>
+            </div>
+            <div class="check">
+                <label for=""><input type="radio" name="choice" value="user">user</label>
+                <label for=""><input type="radio" name="choice" value="admin">admin</label>
+            </div>
+            <div style="display: flex; align-items: center;justify-content: center;">
+                <input type="submit" name="submit" id="submit">
+            </div>
+        </div>
+    </form>
+</body>
+</html>
